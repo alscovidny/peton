a05f2b86a0ad4de1cedc6527351e5ada8f9d6b10
Alexander <sagaradze96@gmail.com>
2021-05-12 00:25:25 +0300

    Сделали тестирование программы на данных из примера

--- task1.py
+++ task1.py
@@ -1,16 +1,12 @@
-#для тестирования программы создадим списочек
+duration_seconds = 400153
 
-#duration_list = [60, 80, 10000, 5009, 543]
-
-duration = 124123
-
-if not duration // 60:
-	seconds = duration
+if not duration_seconds // 60:
+	seconds = duration_seconds
 	print('seconds: ',seconds)
 
 else:
-	minutes = duration // 60
-	seconds = duration % 60
+	minutes = duration_seconds // 60
+	seconds = duration_seconds % 60
 	if not minutes // 60:
 		print(f'{minutes} мин {seconds} сек')
 	else:
@@ -22,3 +18,31 @@ else:
 			days = hours // 24
 			hours = hours % 24
 			print(f'{days} дн {hours} час {minutes} мин {seconds} сек')
+
+#ниже закоменчена тестировочная версия программы
+
+'''
+duration_list = [53, 153, 4153, 400153]
+
+for duration in duration_list:
+	print(f'current duration is {duration} seconds')
+	if not duration // 60:
+		seconds = duration
+		print('seconds: ',seconds)
+
+	else:
+		minutes = duration // 60
+		seconds = duration % 60
+		if not minutes // 60:
+			print(f'{minutes} мин {seconds} сек')
+		else:
+			hours = minutes // 60
+			minutes = minutes % 60
+			if not hours // 24:
+				print(f'{hours} час {minutes} мин {seconds} сек')
+			else:
+				days = hours // 24
+				hours = hours % 24
+				print(f'{days} дн {hours} час {minutes} мин {seconds} сек')
+
+'''
