cef35b9ad541e4f09713d34cd50791410f1c6d56
Alexander <sagaradze96@gmail.com>
2021-05-20 21:15:16 +0300

    сделана v2 5 задания

--- task_3_5.py
+++ task_3_5.py
@@ -1,42 +1,43 @@
-from random import shuffle
+from random import shuffle, choice
 
-def getjokes(number_jokes):
+def getjokes(number_jokes, unique_jokes = False):
+	"""
+	return 
+	"""
+	
+	nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+	adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+	adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
-	nouns = [
-		'костюм', 'гимнаст', 'аргумент', 'дед',
-		'бумер', 'секс', 'жонглёр', 'кузовок'
-		]
+	def make_a_joke():
+		joke = (
+			choice(nouns), choice(adverbs), choice(adjectives)
+			)
+		print(' '.join(joke))
 
-	adverbs = [
-		'НЕ ОЧЕНЬ', 'быстро', 'Т О К С И Ч Н О', 'высоко',
-		'смутно', 'однозначно', 'никогда', 'послезавтра'
-		]
+	def make_unique_jokes():
 
-	adjectives = [
-		'чёрный', 'мягкий', 'столетний', 'опоздавший',
-		'модный', 'многообещающий', 'прошедший', 'неподъёмный'
-		]
+		shuffle(nouns)
+		shuffle(adverbs)
+		shuffle(adjectives)
 
-	shuffle(nouns)
-	shuffle(adverbs)
-	shuffle(adjectives)
-
-	if str(number_jokes).isdigit():
-
-		number_jokes = int(number_jokes)
 		joke_database = list(map(lambda x: ' '.join(x), zip(nouns,adverbs,adjectives)))
 
-		if number_jokes >= max(len(nouns), len(adverbs), len(adjectives)):
-			
+		if int(number_jokes) >= max(len(nouns), len(adverbs), len(adjectives)):
 			print('выведено макисмально возможное количество шуток:')
 			print(joke_database)
-		elif number_jokes > 0:
+
+		elif int(number_jokes) > 0:
 			print(joke_database[:number_jokes])
 
-	else:
-		print('введено некорректное количество шуток')
+	if str(number_jokes).isdigit():
+		print(True)
+
+	make_unique_jokes()
+
+	# else:
+	# 	print('введено некорректное количество шуток')
 
 
-a = input()
 
 getjokes(9)
