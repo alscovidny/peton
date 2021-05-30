3d7d99f22b1c203c9f8fa31d2bc0cad032f24eb7
Alexander <sagaradze96@gmail.com>
2021-05-30 23:40:36 +0300

    сделал по красоте номер 3

--- task_6_3.py
+++ task_6_3.py
@@ -1,35 +1,34 @@
 import sys
 import json
 
-j = 0
+flag = False
+
+def hobby_more_than_users(flag):
+	if flag:
+		sys.exit(1)
 
 with open("users_hobby.txt",'w', encoding = 'utf-8') as output:
 
-	users = open('users.csv','r', encoding = 'utf-8')
+	users = open('users2.csv','r', encoding = 'utf-8')
 	hobby = open('hobby.csv','r', encoding = 'utf-8')
 		
 	database = {}
-	hobby_list = []
 
-	# for line in hobby:
-
-	# 	hobby_list.append(line.strip().split(','))
-
-	print(len(hobby_list))
-	print(hobby_list)
+	hobby_gen = (line.strip().split(',') for line in hobby)
 
 	for line in users:
-		print(j)
-		if j >= len(hobby_list):
+
+		try:
+			elem = next(hobby_gen)
+			database[' '.join(line.strip().split(','))] = elem
+		
+		except StopIteration:
 			database[' '.join(line.strip().split(','))] = None
-		else:
-			database[' '.join(line.strip().split(','))] = hobby_list[j]
-		j += 1
+			if not flag:
+				flag = True
 	
 	users.close()
 	hobby.close()	
 	
 	json.dump(database, output, ensure_ascii = False, indent = 4)
-
-	if j < len(hobby_list):
-		sys.exit(1)
+	hobby_more_than_users(flag)
