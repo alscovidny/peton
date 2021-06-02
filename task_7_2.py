1a235a0e305a7d32ce350214ba59c54fe553c9fb
Alexander <sagaradze96@gmail.com>
2021-06-03 01:28:26 +0300

    подготовил задание 2

--- task_7_2.py
+++ task_7_2.py
@@ -1,7 +1,5 @@
 import os
 
-
-
 depth = 0
 string_dir = os.getcwd()
 
@@ -12,26 +10,28 @@ with open('config.yaml','r', encoding = 'utf-8-sig') as f:
 		if depth == 0:
 			string_dir += r'\{}'.format(line.strip())
 			depth += 1
-			os.makedirs(string_dir)
-			print(string_dir)
+			try:
+				os.makedirs(string_dir)
+			except FileExistsError:
+				pass
 
 		elif depth <= line.count('    '):
 			depth = line.count('    ')
 
-			if line.strip().endswith('.py') or line.strip().endswith('.html'):
-				
-				print(string_dir)
+			if line.strip().endswith('.py') or line.strip().endswith('.html'):				
 				os.chdir(string_dir)
-				print('текущая директория: ', os.getcwd())
-				print(line.strip())
-				f = open(line.strip(), 'x', encoding = 'utf-8')
-				f.close()
+				try:
+					with open(line.strip(), 'x', encoding = 'utf-8'):
+						pass
+				except FileExistsError:
+					pass
 			
 			else:
-
 				string_dir = string_dir + r'\{}'.format(line.strip())
-				os.makedirs(string_dir)
-				print(string_dir)
+				try:
+					os.makedirs(string_dir)
+				except FileExistsError:
+					pass
 
 		elif depth > line.count('    '):
 
@@ -41,5 +41,7 @@ with open('config.yaml','r', encoding = 'utf-8-sig') as f:
 				string_dir = string_dir[ : string_dir.rfind("\\") ]
 			
 			string_dir += r'\{}'.format(line.strip())
-			os.makedirs(string_dir)
-			print(string_dir)
+			try:
+				os.makedirs(string_dir)
+			except FileExistsError:
+				pass
