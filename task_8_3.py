1305756b8aae042cdad71d2d45f5c4cdb006342f
Alexander <sagaradze96@gmail.com>
2021-06-08 00:14:10 +0300

    Сделал задание 4. Подготовил к сдаче

--- task_8_3.py
+++ task_8_3.py
@@ -1,7 +1,6 @@
 from functools import wraps
 
 def type_logger(function):
-
 	@wraps(function)
 	def wrapper(*args, **kwargs):
 		if args:
@@ -17,29 +16,5 @@ def type_logger(function):
 def calc_cube(x = 1):
 	return x ** 3
 
-calc_cube(2, 3, 4, 5)
 
-# def simple_cache(func):
-#    cache = {}
-
-#    def wrapper(*args):
-#        # nonlocal cache
-#        key = str(*args)
-#        if key not in cache:
-#            cache[key] = func(*args)
-#        return cache[key]
-
-#    return wrapper
-
-
-# @simple_cache
-# def render_input(field):
-#    print(f"call render_input('{field}')")
-#    return f'<input id="id_{field}" type="text" name="{field}">'
-
-# username_f = render_input('username')
-# password_f = render_input('password')
-# username_f_2 = render_input('username')
-# print(username_f)
-# print(password_f)
-# print(username_f_2)
+calc_cube(2.2, 3, 4, 5)
